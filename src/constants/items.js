35f129c58e3eaac54409785d601f87c9081b5451
kathyavini <kathyavini@gmail.com>
2022-05-19 18:42:33 -0700

    Better solution for context.require issue

--- src/constants/items.js
+++ src/constants/items.js
@@ -1,38 +1,32 @@
-import images from './images.json';
-
-// Run this first to generate an object you can copy-paste into the above .json file. Yes, it's a little ridiculous, sorry
-// import imageObject from '../utils/makeImgObj';
-// console.log(imageObject);
-
 export const items = [
   {
     name: 'Green Tea',
-    image: images.green,
-    imageAlt: images.green2,
+    image: require('../assets/green.jpg'),
+    imageAlt: require('../assets/green2.jpg'),
     description: 'Light and subtle',
     price: '12.50',
     id: 1,
   },
   {
     name: 'Black Tea',
-    image: images.black,
-    imageAlt: images.black2,
+    image: require('../assets/black.jpg'),
+    imageAlt: require('../assets/black2.jpg'),
     description: 'Smooth and Bright',
     price: '12.50',
     id: 2,
   },
   {
     name: 'Matcha',
-    image: images.matcha,
-    imageAlt: images.matcha2,
+    image: require('../assets/matcha.jpg'),
+    imageAlt: require('../assets/matcha2.jpg'),
     description: 'Vibrant and Bold',
     price: '14.00',
     id: 3,
   },
   {
     name: 'Pu-erh Tea',
-    image: images.puer,
-    imageAlt: images.puer2,
+    image: require('../assets/puer.jpg'),
+    imageAlt: require('../assets/puer2.jpg'),
     description: 'Rich and Fragrant',
     price: '16.50',
     id: 4,
